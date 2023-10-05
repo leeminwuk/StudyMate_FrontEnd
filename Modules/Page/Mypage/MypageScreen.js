ed26cd8c75ce18618cd22013a763f253247e9174
leeminwuk <lmo2914@gmail.com>
2023-10-05 13:59:39 +0900

    마이페이지 구현

--- Modules/Page/Mypage/MypageScreen.js
+++ Modules/Page/Mypage/MypageScreen.js
@@ -1,10 +1,87 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Image } from "react-native";
+import Svg, { Rect } from "react-native-svg";
+import styles from "./styles";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import LongCustomTuner from "../../LongCustomTuner";
+import { useNavigation } from "@react-navigation/native";
 
 const MypageScreen = () => {
+  const navigation = useNavigation();
+
+  const handlenotificationbell = () => {
+    navigation.navigate("Notification");
+  };
+
   return (
-    <View>
-      <Text>This is the Mypage screen.</Text>
+    <View style={{ flex: 1 }}>
+      <View
+        style={[
+          styles.title,
+          {
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          },
+        ]}
+      >
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Svg width="3" height="22">
+            <Rect
+              width="3"
+              height="22"
+              fill="#468CE7"
+              stroke="#468CE7"
+              strokeWidth="1"
+            />
+          </Svg>
+          <Text style={styles.titleText}>마이페이지</Text>
+        </View>
+        <TouchableOpacity onPress={handlenotificationbell}>
+          <Image
+            source={require("../../../assets/bell.png")}
+            style={styles.bellImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.circleContainer}>
+        <View style={styles.circleImage}>
+          <Image
+            source={require("../../../assets/person.png")}
+            style={styles.personImage}
+          />
+        </View>
+      </View>
+      <View style={[styles.nicknameContainer, { justifyContent: "center" }]}>
+        <Text style={styles.nicknameText}>이민욱 님</Text>
+        <TouchableOpacity>
+          <Image
+            source={require("../../../assets/nextbutton.png")}
+            style={{ width: 40, height: 40 }}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.hr}></View>
+      <TouchableOpacity style={styles.appSettingContainer}>
+      <Text style={styles.appSettingText}>
+          App설정
+        </Text>
+        <Image
+          source={require("../../../assets/appsettingbutton.png")}
+          style={styles.appSettingButton}
+        />
+      </TouchableOpacity>
+      <View style={styles.hr}></View>
+      <TouchableOpacity style={styles.appSettingContainer}>
+      <Text style={styles.appSettingText}>
+          고객센터
+        </Text>
+        <Image
+          source={require("../../../assets/appsettingbutton.png")}
+          style={styles.appSettingButton}
+        />
+      </TouchableOpacity>
+      <View style={styles.hr}></View>
     </View>
   );
 };
---
+++ Modules/Page/Mypage/styles.js
@@ -0,0 +1,84 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+title: {
+    marginLeft: 21,
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  titleText: {
+    marginLeft: 5,
+    color: "#000",
+    fontSize: 18,
+    fontStyle: "normal",
+    fontWeight: "600",
+    lineHeight: 22,
+  },
+  bellImage: {
+    width: 28,
+    height: 28,
+    marginRight: 21,
+  },
+    circleContainer:{
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 112,
+    },
+  circleImage:{
+    width: 116,
+    height: 116,
+    borderRadius: 58,
+    borderColor: "#ECECEC",
+    borderWidth: 3,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  personImage:{
+    width: 66,
+    height: 70,
+  },
+    nicknameContainer:{
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        marginTop: 44,
+        marginLeft: 24,
+    },
+    nicknameText:{
+        fontSize: 24,
+        fontWeight: "700",
+        color: "#000",
+        letterSpacing: -0.5,
+    },
+    hr:{
+        width: 344,
+        height: 1,
+        backgroundColor: "#A4A4A4",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 24,
+        marginLeft: 24,
+    },
+    appSettingContainer:{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginTop: 24,
+        marginLeft: 48,
+        marginRight: 40,
+    },
+    appSettingText:{
+        letterSpacing: -0.5,
+        fontSize: 14,
+        fontWeight: "700",
+        color: "#969696",
+        letterSpacing: -0.5,
+    },
+    appSettingButton:{
+        width: 40,
+        height: 40,
+    },
+});
+export default styles;
